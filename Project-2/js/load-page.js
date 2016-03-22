bbe6c7c7c187888644b8acfd88b0f9f0bc655c0a
phanduc0105 <phanduc0105@gmail.com>
2016-03-22 22:13:13 +0700

    update load page
    mở bằng visual studio mới được nhá

---
+++ Project-2/js/load-page.js
@@ -0,0 +1,610 @@
+﻿//Kế toán - Kiểm toán
+$(document).ready(function () {
+    $(".kt").click(function () {
+        $("#content").load("page/ke-toan-kiem-toan.html");
+    });
+});
+
+//Kiến trúc - TK nội thất
+$(document).ready(function () {
+    $(".kt-tknt").click(function () {
+        $("#content").load("page/kien-truc-tk-noi-that.html");
+    });
+});
+
+//Nhân viên kinh doanh
+$(document).ready(function () {
+    $(".nvkd").click(function () {
+        $("#content").load("page/nhan-vien-kinh-doanh.html");
+    });
+});
+
+//Hành chính - Văn phòng
+$(document).ready(function () {
+    $(".hc-vp").click(function () {
+        $("#content").load("page/hanh-chinh-van-phong.html");
+    });
+});
+
+//Xây dựng
+$(document).ready(function () {
+    $(".xd").click(function () {
+        $("#content").load("page/xay-dung.html");
+    });
+});
+
+//Marketing - PR
+$(document).ready(function () {
+    $(".mkt-pr").click(function () {
+        $("#content").load("page/marketing-pr.html");
+    });
+});
+
+//Tư vấn
+$(document).ready(function () {
+    $(".tv").click(function () {
+        $("#content").load("page/tu-van.html");
+    });
+});
+
+//Kỹ thuật
+$(document).ready(function () {
+    $(".kth").click(function () {
+        $("#content").load("page/ky-thuat.html");
+    });
+});
+
+//Bán hàng
+$(document).ready(function () {
+    $(".bh").click(function () {
+        $("#content").load("page/ban-hang.html");
+    });
+});
+
+//IT phần mềm
+$(document).ready(function () {
+    $(".itpm").click(function () {
+        $("#content").load("page/it-phan-mem.html");
+    });
+});
+
+//Báo chí - Truyền hình
+$(document).ready(function () {
+    $(".bc-th").click(function () {
+        $("#content").load("page/bao-chi-truyen-hinh.html");
+    });
+});
+
+//Bảo hiểm
+$(document).ready(function () {
+    $(".bhi").click(function () {
+        $("#content").load("page/bao-hiem.html");
+    });
+});
+
+//Bảo vệ
+$(document).ready(function () {
+    $(".bv").click(function () {
+        $("#content").load("page/bao-ve.html");
+    });
+});
+
+//Biên - Phiên dịch
+$(document).ready(function () {
+    $(".bpd").click(function () {
+        $("#content").load("page/bien-phien-dich.html");
+    });
+});
+
+//Bưu chính
+$(document).ready(function () {
+    $(".bc").click(function () {
+        $("#content").load("page/buu-chinh.html");
+    });
+});
+
+//Chứng khoán - Vàng
+$(document).ready(function () {
+    $(".ck-v").click(function () {
+        $("#content").load("page/chung-khoan-vang.html");
+    });
+});
+
+//Cơ khí - Chế tạo
+$(document).ready(function () {
+    $(".ck-ct").click(function () {
+        $("#content").load("page/co-khi-che-tao.html");
+    });
+});
+
+//Công nghiệp
+$(document).ready(function () {
+    $(".cn").click(function () {
+        $("#content").load("page/cong-nghiep.html");
+    });
+});
+
+//Dầu khí - Hóa chất
+$(document).ready(function () {
+    $(".dk-hc").click(function () {
+        $("#content").load("page/dau-khi-hoa-chat.html");
+    });
+});
+
+//Đầu tư
+$(document).ready(function () {
+    $(".dt").click(function () {
+        $("#content").load("page/dau-tu.html");
+    });
+});
+
+//Dệt may - Da giày
+$(document).ready(function () {
+    $(".dm-dg").click(function () {
+        $("#content").load("page/det-may-da-giay.html");
+    });
+});
+
+//Dịch vụ
+$(document).ready(function () {
+    $(".dv").click(function () {
+        $("#content").load("page/dich-vu.html");
+    });
+});
+
+//Điện tử viễn thông
+$(document).ready(function () {
+    $(".dtvt").click(function () {
+        $("#content").load("page/dien-tu-vien-thong.html");
+    });
+});
+
+//Điện - Điện tử
+$(document).ready(function () {
+    $(".ddt").click(function () {
+        $("#content").load("page/dien-dien-tu.html");
+    });
+});
+
+//Du lịch
+$(document).ready(function () {
+    $(".dl").click(function () {
+        $("#content").load("page/du-lich.html");
+    });
+});
+
+//Game
+$(document).ready(function () {
+    $(".game").click(function () {
+        $("#content").load("page/game.html");
+    });
+});
+
+//Giáo dục - Đào tạo
+$(document).ready(function () {
+    $(".gd-dt").click(function () {
+        $("#content").load("page/giao-duc-dao-tao.html");
+    });
+});
+
+//Hàng gia dụng
+$(document).ready(function () {
+    $(".hgd").click(function () {
+        $("#content").load("page/hang-gia-dung.html");
+    });
+});
+
+//Hàng hải
+$(document).ready(function () {
+    $(".hh").click(function () {
+        $("#content").load("page/hang-hai.html");
+    });
+});
+
+//Hàng không
+$(document).ready(function () {
+    $(".hk").click(function () {
+        $("#content").load("page/hang-khong.html");
+    });
+});
+
+//Hóa học - Sinh học
+$(document).ready(function () {
+    $(".hh-sh").click(function () {
+        $("#content").load("page/giao-duc-dao-tao.html");
+    });
+});
+
+//Hoạch định - Dự án
+$(document).ready(function () {
+    $(".hd-da").click(function () {
+        $("#content").load("page/hoach-dinh-du-an.html");
+    });
+});
+
+//In ấn - Xuất bản
+$(document).ready(function () {
+    $(".ia-xb").click(function () {
+        $("#content").load("page/in-an-xuat-ban.html");
+    });
+});
+
+//IT phần cứng - Mạng
+$(document).ready(function () {
+    $(".itpc-m").click(function () {
+        $("#content").load("page/it-phan-cung-mang.html");
+    });
+});
+
+//Kinh doanh bất động sản
+$(document).ready(function () {
+    $(".kdbds").click(function () {
+        $("#content").load("page/kinh-doanh-bat-dong-san.html");
+    });
+});
+
+//Khách sạn - Nhà hàng
+$(document).ready(function () {
+    $(".ks-nh").click(function () {
+        $("#content").load("page/khach-san-nha-hang.html");
+    });
+});
+
+//Mỹ phẩm - trang sức
+$(document).ready(function () {
+    $(".mp-ts").click(function () {
+        $("#content").load("page/my-pham-trang-suc.html");
+    });
+});
+
+//Ngân hàng
+$(document).ready(function () {
+    $(".nh").click(function () {
+        $("#content").load("page/ngan-hang.html");
+    });
+});
+
+//Ngành nghề khác
+$(document).ready(function () {
+    $(".nnk").click(function () {
+        $("#content").load("page/nganh-nghe-khac.html");
+    });
+});
+
+
+//Nghệ thuật điện ảnh
+$(document).ready(function () {
+    $(".ntda").click(function () {
+        $("#content").load("page/nghe-thuat-dien-anh.html");
+    });
+});
+
+//Nhân sự
+$(document).ready(function () {
+    $(".ns").click(function () {
+        $("#content").load("page/nhan-su.html");
+    });
+});
+
+//Nông - Lâm - Ngư nghiệp
+$(document).ready(function () {
+    $(".n-l-nn").click(function () {
+        $("#content").load("page/nong-lam-ngu-nghiep.html");
+    });
+});
+
+//Ô tô - Xe máy
+$(document).ready(function () {
+    $(".ot-xm").click(function () {
+        $("#content").load("page/o-to-xe-may.html");
+    });
+});
+
+//Pháp lý
+$(document).ready(function () {
+    $(".pl").click(function () {
+        $("#content").load("page/phap-ly.html");
+    });
+});
+
+//Quan hệ đối ngoại
+$(document).ready(function () {
+    $(".qhdn").click(function () {
+        $("#content").load("page/quan-he-doi-ngoai.html");
+    });
+});
+
+//Quản trị kinh doanh
+$(document).ready(function () {
+    $(".qtkd").click(function () {
+        $("#content").load("page/quan-tri-kinh-doanh.html");
+    });
+});
+
+//Thực phẩm - Đồ uống
+$(document).ready(function () {
+    $(".tp-du").click(function () {
+        $("#content").load("page/thuc-pham-do-uong.html");
+    });
+});
+
+//Thiết kế đồ họa web
+$(document).ready(function () {
+    $(".web").click(function () {
+        $("#content").load("page/thiet-ke-do-hoa-web.html");
+    });
+});
+
+//Thiết kế - Mỹ thuật
+$(document).ready(function () {
+    $(".kt-mt").click(function () {
+        $("#content").load("page/thiet-ke-my-thuat.html");
+    });
+});
+
+//Thời trang
+$(document).ready(function () {
+    $(".tt").click(function () {
+        $("#content").load("page/thoi-trang.html");
+    });
+});
+
+//Thủ cong mỹ nghệ
+$(document).ready(function () {
+    $(".tc-mn").click(function () {
+        $("#content").load("page/thu-cong-my-nghe.html");
+    });
+});
+
+//Thư ký - Trợ lý
+$(document).ready(function () {
+    $(".kt-tl").click(function () {
+        $("#content").load("page/thu-ky-tro-ly.html");
+    });
+});
+
+//Thương mại điện tử
+$(document).ready(function () {
+    $(".tmdt").click(function () {
+        $("#content").load("page/thuong-mai-dien-tu.html");
+    });
+});
+
+//Tiếp thị - Quảng cáo
+$(document).ready(function () {
+    $(".tt-qc").click(function () {
+        $("#content").load("page/tiep-thi-quang-cao.html");
+    });
+});
+
+//Tổ chức sự kiện - Quà tặng
+$(document).ready(function () {
+    $(".tcsk-qt").click(function () {
+        $("#content").load("page/to-chuc-su-kien-qa-tang.html");
+    });
+});
+
+//Vận tải - Lái xe
+$(document).ready(function () {
+    $(".vt-lx").click(function () {
+        $("#content").load("page/van-tai-lai-xe.html");
+    });
+});
+
+//Vật tư - Thiết bị
+$(document).ready(function () {
+    $(".vt-tb").click(function () {
+        $("#content").load("page/vat-tu-thiet-bi.html");
+    });
+});
+
+//Xuất - Nhập khẩu
+$(document).ready(function () {
+    $(".x-nk").click(function () {
+        $("#content").load("page/xuat-nhap-khau.html");
+    });
+});
+
+//Y tế - Dược
+$(document).ready(function () {
+    $(".yt-d").click(function () {
+        $("#content").load("page/y-te-duoc.html");
+    });
+});
+
+//Quận 1
+$(document).ready(function () {
+    $(".q1").click(function () {
+        $("#content").load("page/quan-1.html");
+    });
+});
+
+//Quận 2
+$(document).ready(function () {
+    $(".q2").click(function () {
+        $("#content").load("page/quan-2.html");
+    });
+});
+
+//Quận 3
+$(document).ready(function () {
+    $(".q3").click(function () {
+        $("#content").load("page/quan-3.html");
+    });
+});
+
+//Quận 4
+$(document).ready(function () {
+    $(".q4").click(function () {
+        $("#content").load("page/quan-4.html");
+    });
+});
+
+//Quận 5
+$(document).ready(function () {
+    $(".q5").click(function () {
+        $("#content").load("page/quan-5.html");
+    });
+});
+
+//Quận 6
+$(document).ready(function () {
+    $(".q6").click(function () {
+        $("#content").load("page/quan-6.html");
+    });
+});
+
+//Quận 7
+$(document).ready(function () {
+    $(".q7").click(function () {
+        $("#content").load("page/quan-7.html");
+    });
+});
+
+//Quận 8
+$(document).ready(function () {
+    $(".q8").click(function () {
+        $("#content").load("page/quan-8.html");
+    });
+});
+
+//Quận 9
+$(document).ready(function () {
+    $(".q9").click(function () {
+        $("#content").load("page/quan-9.html");
+    });
+});
+
+//Quận 2
+$(document).ready(function () {
+    $(".q10").click(function () {
+        $("#content").load("page/quan-10.html");
+    });
+});
+
+//Quận 11
+$(document).ready(function () {
+    $(".q11").click(function () {
+        $("#content").load("page/quan-11.html");
+    });
+});
+
+//Quận 12
+$(document).ready(function () {
+    $(".q12").click(function () {
+        $("#content").load("page/quan-12.html");
+    });
+});
+
+//Quận Bình Tân
+$(document).ready(function () {
+    $(".qbt").click(function () {
+        $("#content").load("page/quan-binh-tan.html");
+    });
+});
+
+//Quận Bình Thạnh
+$(document).ready(function () {
+    $(".qbth").click(function () {
+        $("#content").load("page/quan-binh-thanh.html");
+    });
+});
+
+//Quận Gò Vấp
+$(document).ready(function () {
+    $(".qgv").click(function () {
+        $("#content").load("page/quan-go-vap.html");
+    });
+});
+
+//Quận Phú Nhuận
+$(document).ready(function () {
+    $(".q2").click(function () {
+        $("#content").load("page/quan-phu-nhuan.html");
+    });
+});
+
+//Quận Tân Bình
+$(document).ready(function () {
+    $(".qtb").click(function () {
+        $("#content").load("page/quan-tan-binh.html");
+    });
+});
+
+//Quận Tân Phú
+$(document).ready(function () {
+    $(".qtp").click(function () {
+        $("#content").load("page/quan-tan-phu.html");
+    });
+});
+
+//Quận Thủ Đức
+$(document).ready(function () {
+    $(".qtd").click(function () {
+        $("#content").load("page/quan-thu-duc.html");
+    });
+});
+
+//Huyện Bình Chánh
+$(document).ready(function () {
+    $(".hbc").click(function () {
+        $("#content").load("page/huyen-binh-chanh.html");
+    });
+});
+
+//Huyện củ chi
+$(document).ready(function () {
+    $(".qtd").click(function () {
+        $("#content").load("page/quan-thu-duc.html");
+    });
+});
+
+//Huyện Hóc Môn
+$(document).ready(function () {
+    $(".hhm").click(function () {
+        $("#content").load("page/huyen-hoc-mon.html");
+    });
+});
+
+//Đăng tin tuyển dụng
+$(document).ready(function () {
+    $(".dttd").click(function () {
+        $("#content").load("page/dang-tin-tuyen-dung.html");
+    });
+});
+
+//Tạo hồ sơ ứng viên
+$(document).ready(function () {
+    $(".thsuv").click(function () {
+        $("#content").load("page/tao-ho-so-ung-vien.html");
+    });
+});
+
+//Tin tuyển dụng
+$(document).ready(function () {
+    $(".ttd").click(function () {
+        $("#content").load("page/tin-tuyen-dung.html");
+    });
+});
+
+//Ứng viên
+$(document).ready(function () {
+    $(".uv").click(function () {
+        $("#content").load("page/ung-vien.html");
+    });
+});
+
+//Giới thiệu
+$(document).ready(function () {
+    $(".gt").click(function () {
+        $("#content").load("page/gioi-thieu.html");
+    });
+});
+
+
+//Liên hệ
+$(document).ready(function () {
+    $(".lh").click(function () {
+        $("#content").load("page/lien-he.html");
+    });
+});
